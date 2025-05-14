7bef87b3e6789d90e4095be240c7c98d6c2a7924
timur <timurfays@gmail.com>
2025-05-14 09:18:19 +0300

    Update requirements-base.txt with dependencies for graph_visualization.ipynb nd new permision fixer

--- fix-permisions.sh
+++ fix-permisions.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
+# 1. Права для существующих файлов/папок
+sudo chmod -R a+rwX .
 
-# Установить rwxrwxrwx для директорий
-sudo find . -type d -exec chmod 0777 {} \;
-# Установить rwxrwxrwx для исполняемых файлов (100755)
-sudo find . -type f -perm -u+x -exec chmod 0777 {} \;
-# Установить rw-rw-rw- для неисполняемых файлов (100644)
-sudo find . -type f ! -perm -u+x -exec chmod 0666 {} \;
-# Установить setgid для наследования группы
-sudo find . -type d -exec chmod g+s {} \;
-# Установить дефолтные ACL для новых файлов/папок
-sudo setfacl -R -m d:u::rwx,d:g::rwx,d:o::rwx,d:m::rw .
+# 2. Наследование группы
+sudo chmod g+s .
+
+# 3. Настройка ACL для новых файлов и папок (исправленная версия)
+sudo setfacl -R -d -m o::rwx .
